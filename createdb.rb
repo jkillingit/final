ffc97d47c5b52e8903dc5f042c6e2c23a68b1185
jkillingit <justin.r.killion@gmail.com>
2020-06-06 20:43:22 +0000

    db and seed complete

--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,35 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :restaurants do
   primary_key :id
-  String :title
+  String :restaurant
   String :description, text: true
-  String :date
+  String :phone
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :comments do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :restaurant_id
+  foreign_key :user_id
+  String :comment, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+restaurants_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+restaurants_table.insert(restaurant: "Ryo Sushi", 
+                    description: "Ryo Sushi is a great sushi spot in the middle of the loop!  The fish is super fresh and portions generous.  I love the green dragon, red dragon, rainbow and tuna avocado rolls!",
+                    phone: "(312) 409-8888",
+                    location: "62 E Madison St, Chicago, IL 60602")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+restaurants_table.insert(restaurant: "Happy Camper", 
+                    description: "Happy Camper is a great local mini-chain with two locations, one in Old Town and another in Wrigleyville, and a sister restaurant called Homeslice.  They specialize in thin pizzas with fluffy crusts and less traditional toppings.  During the quarantine they put together some great takeout specials like the party for one: a small pepperoni pizza, cookie dough tub, a six pack of beers and a Smirnoff Ice.",
+                    phone: "(312) 344-1634",
+                    location: "1209 N Wells St, Chicago, IL 60610")
